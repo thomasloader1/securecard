f3d204cb72ee9b9f92a4e4cb6a97ad41d0106cac
Tomas Gomez <tomas.gomez@aufieroinformatica.com.ar>
2020-02-19 11:02:12 -0300

    Comienzo

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$('[data-toggle="popover-hover-1"]').popover({
+    html: true,
+    trigger: 'hover',
+    placement: 'top',
+    content: function() { return '<p class="font-weight-bold space-mono font-18">Seguridad de la red</p><p class="proxima-nova">Entre los ejemplos de piratas informáticos de seguridad de la red cabe citar la explotación de vulnerabilidades como los puntos de acceso abiertos, los certificados SSL inseguros o mal configurados, o las vulnerabilidades de las bases de datos y los agujeros de seguridad que pueden derivarse de la falta de medidas de seguridad adecuadas.</p>'; }
+});
+$('[data-toggle="popover-hover-2"]').popover({
+    html: true,
+    trigger: 'hover',
+    placement: 'top',
+    content: function() { return '<p class="font-weight-bold space-mono font-18">Salud del DNS</p><p class="proxima-nova">La plataforma SecurityScorecard mide múltiples configuraciones de DNS, como las configuraciones de OpenResolver, así como la presencia de configuraciones recomendadas como DNSSEC, SPF, DKIM y DMARC.</p>'; }
+});
+$('[data-toggle="popover-hover-3"]').popover({
+    html: true,
+    trigger: 'hover',
+    placement: 'top',
+    content: function() { return '<p class="font-weight-bold space-mono font-18">Cadencia de parcheo</p><p class="proxima-nova">La diligencia con la que una compañía está parcheando sus sistemas operativos, servicios, aplicaciones, software y hardware de manera oportuna.</p>'; }
+});
+$('[data-toggle="popover-hover-4"]').popover({
+    html: true,
+    trigger: 'hover',
+    placement: 'top',
+    content: function() { return '<p class="font-weight-bold space-mono font-18">Seguridad del Endpoint</p><p class="proxima-nova">La seguridad de los Endpoint se refiere a la protección que implica en relación con las computadoras portátiles, las computadoras de escritorio, los dispositivos móviles y todos los dispositivos de los empleados que acceden a la red de esa empresa.</p>'; }
+});
+$('[data-toggle="popover-hover-5"]').popover({
+    html: true,
+    trigger: 'hover',
+    placement: 'top',
+    content: function() { return '<p class="font-weight-bold space-mono font-18">Reputación de la IP</p><p class="proxima-nova">El sistema de sumideros SecurityScorecard ingiere millones de señales de malware de las infraestructuras de mando y control (C2) comandadas de todo el mundo. Las direcciones IP infectadas entrantes se procesan y se atribuyen a empresas corporativas a través de nuestro algoritmo de atribución de IP. La cantidad y la duración de las infecciones de malware se utilizan como factor determinante para estos cálculos, proporcionando un punto de datos para la evaluación general de la Reputación IP de una organización, junto con otras técnicas de evaluación.</p>'; }
+});
+$('[data-toggle="popover-hover-6"]').popover({
+    html: true,
+    trigger: 'hover',
+    placement: 'top',
+    content: function() { return '<p class="font-weight-bold space-mono font-18">Seguridad de aplicaciones web</p><p class="proxima-nova">Entre los ejemplos de vulnerabilidades detectadas se encuentran los ataques de Cross-site Scripting (XSS) o de inyección SQL.</p>'; }
+});
+$('[data-toggle="popover-hover-7"]').popover({
+    html: true,
+    trigger: 'hover',
+    placement: 'top',
+    content: function() { return '<p class="font-weight-bold space-mono font-18">Puntuación del cubo</p><p class="proxima-nova">El factor de puntuación del cubo es el indicador de amenazas de propiedad de SecurityScorecard que mide un conjunto de problemas críticos de seguridad y configuración relacionados con los portales administrativos expuestos.</p>'; }
+});
+$('[data-toggle="popover-hover-8"]').popover({
+    html: true,
+    trigger: 'hover',
+    placement: 'top',
+    content: function() { return '<p class="font-weight-bold space-mono font-18">Charla de hackers</p><p class="proxima-nova">El factor "SecurityScorecard Hacker Chatter" recoge continuamente comunicaciones de múltiples corrientes de charlas clandestinas, incluyendo foros de hackers privados o de difícil acceso. Se identifican las organizaciones y los IP que se discuten o se atacan.</p>'; }
+});
+$('[data-toggle="popover-hover-9"]').popover({
+    html: true,
+    trigger: 'hover',
+    placement: 'top',
+    content: function() { return '<p class="font-weight-bold space-mono font-18">Credenciales filtradas</p><p class="proxima-nova">SecurityScorecard identifica toda la información sensible que se expone como parte de una brecha o fuga de datos, volcados de los registradores de teclas, volcados de la base de datos y a través de otros depósitos de información. SecurityScorecard asigna la información a las empresas propietarias de los datos o a las cuentas de correo electrónico asociadas que están conectadas a la información filtrada, evaluando la probabilidad de que una organización sucumba a un incidente de seguridad debido a la información filtrada.</p>'; }
+});
+$('[data-toggle="popover-hover-10"]').popover({
+    html: true,
+    trigger: 'hover',
+    placement: 'top',
+    content: function() { return '<p class="font-weight-bold space-mono font-18">Ingeniería Social</p><p class="proxima-nova">SecurityScorecard identifica una variedad de factores relacionados con la ingeniería social, como los empleados que utilizan la información de sus cuentas corporativas para servicios, por ejemplo, redes sociales, cuentas de servicios, cuentas de finanzas personales y listas de comercialización que pueden ser explotadas. Además, la insatisfacción de los empleados se vigila mediante datos disponibles públicamente.</p>'; }
+});
